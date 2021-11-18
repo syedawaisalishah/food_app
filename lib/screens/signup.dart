bb696783448dbbbdd3255341adc18ba63fcb74b3
FlutLab.io <>
2021-11-18 18:31:16 +0000

    Autosave

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Signup extends StatefullWidget {
+class Signup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
